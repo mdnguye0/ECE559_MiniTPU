3790f10a5157afcd59a2d0a0937284b3e2beced0
mdnguye0 <mdnguye0@umass.edu>
2026-03-04 18:11:26 -0500

    Memory data path

---
+++ modules/act_sram/act_sram.sv
@@ -0,0 +1,31 @@
+module act_sram #(
+    parameter int WORD_WIDTH = 256,
+    parameter int DEPTH = 1024, 
+    parameter int ADDR_WIDTH = $clog2(DEPTH), 
+)
+(
+    input logic clk, 
+    // Use to load image data 
+    input logic w_en, 
+    input logic signed [ADDR_WIDTH -1:0] waddr, 
+    input logic signed [WORD_WIDTH-1:0] wdata, 
+
+    // Use by FIFO to fetch data 
+    input logic r_en, 
+    input logic signed [ADDR_WIDTH-1:0] raddr, 
+    output logic signed[WORD_WIDTH-1:0] rdata
+); 
+
+    logic [WORD_WIDTH-1:0] ram[0:DEPTH-1]; 
+    always_ff @(posedge clk) begin 
+        if (wen) begin 
+            ram[waddr] <= wdata; 
+        end 
+    end 
+
+    always_ff @(posedge clk) begin 
+        if (ren) begin 
+            rdata <= ram[raddr]; 
+        end 
+    end 
+endmodule 
---
+++ modules/input_fifo/input_fifo.sv
@@ -0,0 +1,55 @@
+module input_fifo #(
+    parameter int DATA_WIDTH = 256, 
+    parameter int DEPTH = 16, 
+    parameter int ADDR_WIDTH = $clog2(DEPTH)
+)(
+    input logic clk, 
+    input logic rst_n, 
+
+    input logic wr_en, 
+    input logic [DATA_WIDTH-1:0] wr_data, 
+    output logic full, 
+
+    input logic rd_en, 
+    output logic [DATA_WIDTH-1:0] rd_data, 
+    output logic empty
+); 
+    logic [DATA_WIDTH-1:0] mem[0:DEPTH-1]; 
+    logic [ADDR_WIDTH -1:0] wr_ptr; 
+    logic [ADDR_WIDTH-1:0] rd_ptr; 
+    logic [ADDR_WIDTH:0] count; 
+
+    assign full = (count == DEPTH); 
+    assign empty = (count == 0); 
+
+    always_ff @(posedge clk or negedge rst_n) begin 
+        if (!rst_n) begin 
+            wr_ptr <= 0; 
+        end else if (wr_en && !full) begin 
+            mem [wr_ptr] <= wr_data; 
+            wr_ptr <= wr_ptr + 1; 
+    end 
+    end 
+
+    always_ff @(posedge clk or negedge rst_n) begin 
+        if (!rst_n) begin 
+            rd_ptr <= 0; 
+            rd_data <= 0; 
+        end else if (rd_en && !empty) begin 
+            rd_data <= mem[rd_ptr]; 
+            rd_ptr <= rd_ptr + 1; 
+        end
+    end  
+
+    always_ff @(posedge clk or negedge rst_n) begin 
+        if (!rst_n) begin 
+            count <= 0; 
+        end else begin 
+            case ({wr_en && !full, rd_en && !empty})
+                2'b10: count <= count + 1; 
+                2'b01: count <= count -1; 
+                default: count <= count; 
+            endcase 
+        end
+    end
+endmodule 
